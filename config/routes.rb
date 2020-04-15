405518b0ef54fb91bc9bbcd7a795ab0e3ab0a13a
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-04-15 20:23:53 -0300

    Fazendo os endpoints de busca e de categorias

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories
+    end
+  end
 end
